81d2a7a16ddd32a02f4d43f5e0da90f1c939182d
Steve <stephenhillier@gmail.com>
2017-12-20 10:47:56 -0800

    Unit test for app

---
+++ test/unit/specs/App.spec.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from '@/App'
+
+describe('HelloWorld.vue', () => {
+  it('should render header, input and output components', () => {
+    const Constructor = Vue.extend(App)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('#header').textContent)
+    .to.equal('Sieveplot')
+    expect(vm.$el.querySelector('#input').textContent)
+    .to.contain('input')
+    expect(vm.$el.querySelector('#output').textContent)
+    .to.contain('result')
+  })
+})
