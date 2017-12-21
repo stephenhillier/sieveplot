02dddcff28853782b6d568d7abd34bf781ccbe49
Steve <stephenhillier@gmail.com>
2017-12-21 00:24:22 -0800

    Simple table with dummy data

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,10 +1,70 @@
 <template>
   <article class="message is-success">
     <div class="message-header">
-      <p>Test result</p>
+      <p>Test results</p>
     </div>
     <div class="message-body">
-      
+      <div class="columns">
+        <div class="column">
+
+        </div>
+        <div class="column is-narrow">
+          <div class="box">
+            <table class="table">
+              <thead>
+                <th><abbr title="Nominal size">Size</abbr></th>
+                <th><abbr title="Recorded mass">Mass</abbr></th>
+                <th><abbr title="Percent passing">Passing</abbr></th>
+              </thead>
+              <tfoot>
+                <th></th>
+                <th>Error:</th>
+                <th>0.3%</th>
+              </tfoot>
+              <tbody>
+                <template v-for="sieve in percentPassing">
+                  <tr :key="sieve.size">
+                    <th>{{ sieve.size }}</th>
+                    <td>{{ sieve.mass }}</td>
+                    <td>{{ sieve.passing }} %</td>
+                  </tr>
+                </template>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   </article>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        percentPassing: [
+          {
+            size: 50,
+            mass: 220,
+            passing: 90,
+            sizeUnit: 'mm',
+            massUnit: 'g'
+          },
+          {
+            size: 20,
+            mass: 350,
+            passing: 75,
+            sizeUnit: 'mm',
+            massUnit: 'g'
+          },
+          {
+            size: 16,
+            mass: 155,
+            passing: 65,
+            sizeUnit: 'mm',
+            massUnit: 'g'
+          }
+        ]
+      }
+    }
+  }
+</script>
